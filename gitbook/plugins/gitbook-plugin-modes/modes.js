993f0e1fed6404f7cad110130a4fc0325486647f
pandafulmanda <pandafulmanda@gmail.com>
2016-03-25 01:29:33 -0500

    add built book for gh-pages

---
+++ gitbook/plugins/gitbook-plugin-modes/modes.js
@@ -0,0 +1,44 @@
+require(["gitbook", "jquery", "lodash"], function(gitbook, $, _) {
+
+  function setModes(){
+    var modes = getQueryParams();
+    var $modes;
+
+    if(_.isEmpty(modes)){
+      $modes = $('[data-mode][data-mode-default]');
+    } else {
+      $modes = getAllByModes(modes);
+    }
+
+    $modes.addClass('on');
+  }
+
+  function getAllByModes(modes){
+    var modeSelectors = _.map(modes, function(modeValue, mode){
+      return '[data-mode=' + mode + ']';
+    });
+    var allModes = modeSelectors.join(',');
+    return $(allModes);
+  }
+
+  function getQueryParams(searchString) {
+    var searchString = searchString || window.location.search;
+    if(searchString.search(/\\?/) === 0){
+      searchString = searchString.substring(1);
+    }
+
+    var queryPairs = searchString.split('&');
+    var queryParams = {};
+
+    _.each(queryPairs, function(querySet){
+      var pair = querySet.split('=');
+      if(pair[0] !== ''){
+        queryParams[pair[0]] = pair[1];
+      }
+    });
+
+    return queryParams;
+  }
+
+  gitbook.events.bind("page.change", setModes);
+});
